01749ec4ebc2960bec82b93fcfffcc258a46d21f
glacambre <code@lacamb.re>
2021-10-08 07:42:12 +0200

    Prevent breakage when NVIM_LISTEN_ADDRESS undefined

--- syncwd.zsh
+++ syncwd.zsh
@@ -1,12 +1,14 @@
 
-if [[ ! -v chpwd_functions ]]; then
-  chpwd_functions=()
+if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
+  if [[ ! -v chpwd_functions ]]; then
+    chpwd_functions=()
+  fi
+
+  SYNCWD_PLUGIN_DIR="$(dirname "$0")"
+  function syncwd () {
+    pwd="${(qqq)PWD}"
+    nvim -u NONE -i NONE --headless --cmd "source $SYNCWD_PLUGIN_DIR/syncwd.vim" -c ":call Syncwd('$NVIM_LISTEN_ADDRESS', '$pwd', $$)"
+  }
+
+  chpwd_functions+=syncwd
 fi
-
-SYNCWD_PLUGIN_DIR="$(dirname "$0")"
-function syncwd () {
-  pwd="${(qqq)PWD}"
-  nvim -u NONE -i NONE --headless --cmd "source $SYNCWD_PLUGIN_DIR/syncwd.vim" -c ":call Syncwd('$NVIM_LISTEN_ADDRESS', '$pwd', $$)"
-}
-
-chpwd_functions+=syncwd
